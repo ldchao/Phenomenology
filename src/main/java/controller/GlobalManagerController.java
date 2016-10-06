2c7f18464728ce5db36f26c854dd15fe7674b05e
ldc14 <ldc14@software.nju.edu.cn>
2016-10-06 21:45:06 +0800

    中英文版本切换

---
+++ src/main/java/controller/GlobalManagerController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import ENUM.UniversalState;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by lvdechao on 2016/10/6.
+ */
+@Controller
+public class GlobalManagerController {
+
+    //设为版本
+    @RequestMapping(value = "/setVersion")
+    @ResponseBody
+    public String setVersion(String version,HttpServletRequest request){
+        request.getSession().setAttribute("Version",version);
+        return UniversalState.SUCCEED.toString();
+    }
+
+
+    //得到当前版本
+    @RequestMapping(value = "/getVersion")
+    @ResponseBody
+    public String getVersion(HttpServletRequest request){
+        String version=(String)request.getSession().getAttribute("Version");
+        if(version.equals("null")){
+            version="ch";
+        }
+        return version;
+    }
+
+    //全局搜索
+
+}
